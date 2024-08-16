4ad80c5001104b9cdeaf6708b3f5d0a880e73237
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-15 23:04:34 -0300

    First steps - Initialization, initial settings of development environment and creation of variables

---
+++ Build/aula01.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("Hello World");
+
+const nome = "Fernando";
+const canal = "CFBCursos";
+const curso = "Typescript";
+
+console.log(nome);
+console.log(canal);
+console.log(curso);
---
+++ src/aula01.ts
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+const nome = "Fernando";
+const canal = "CFBCursos";
+const curso = "Typescript";
+
+console.log(nome)
+console.log(canal)
+console.log(curso)
+
+
+
