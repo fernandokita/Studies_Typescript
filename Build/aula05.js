ffd9945ffba63e2d8cb8d3221ca192a8aab2de26
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-17 18:17:41 -0300

    Working about tuples, specifically in arrays.

---
+++ Build/aula05.js
@@ -0,0 +1,6 @@
+// Working about tuples in array
+var myArray = ["book", 300, "pen"]; // Tuples consists in specify the index's value type to assignment.
+myArray.push(200, "food");
+console.log(myArray);
+var myArray_2 = [500, true, "dog"];
+console.log(myArray_2);
---
+++ _src/aula05.ts
@@ -0,0 +1,10 @@
+// Working about tuples in array
+
+let myArray : (string|number|string)[]=["book",300,"pen"] // Tuples consists in specify the index's value type to assignment.
+myArray.push(200,"food")
+console.log(myArray)
+
+let myArray_2 : [number,boolean,string] = [500,true,"dog"]
+console.log(myArray_2)
+
+
