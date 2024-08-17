708a81eafe866b644bbaa1846b68776110120622
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-17 19:05:54 -0300

    Working about basic object's concept - Creation and assignment of employer object's attributes.

---
+++ Build/aula06.js
@@ -0,0 +1,8 @@
+// Working about Object's concept
+var employer = {
+    name: "Marshall",
+    age: 35,
+    totalSales: 2000,
+    saudacao: function (name) { console.log("Hi, my name is" + name + "How can I help you ?"); }
+};
+console.log(employer);
---
+++ _src/aula06.ts
@@ -0,0 +1,10 @@
+// Working about Object's concept
+let employer : Object = {
+    name : "Marshall",
+    age : 35,
+    totalSales : 2000,
+    saudacao:(name:string)=>{console.log("Hi, my name is"+ name + "How can I help you ?")}
+}
+console.log(employer)
+
+
