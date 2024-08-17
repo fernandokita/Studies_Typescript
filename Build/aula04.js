0f155febcf59b38a7836570fead04db4db2a0217
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-16 22:44:49 -0300

    Exploring more array's concept in practice with readonly, insertion and removal's method.

---
+++ Build/aula04.js
@@ -0,0 +1,16 @@
+// Learning and exploring more about array and Readonly array..
+var arrayNumber = [100, 200, 300, 400] // Only works assignment's numeric values
+arrayNumber.push(500) // It will inserts 500 in last index of array
+console.log(arrayNumber)
+
+arrayNumber.unshift(50) // It will inserts 50 in first index of array
+console.log(arrayNumber)
+
+arrayNumber.pop(); // It will removes the last element of array
+console.log(arrayNumber)
+
+arrayNumber.shift() // It will removes the first element of array
+console.log(arrayNumber)
+
+var arrayNumber_ro = [1000, 2000, 3000] // Only will reads the array
+console.log(arrayNumber_ro)
